470c084d63be2e1484ad61dbcb36d0f757af5181
davidcoats <david@getsafetycone.com>
2021-11-30 17:20:57 -0800

    Initial commit of source-stub.

---
+++ source/R5T.S0024.Lib/Code/Classes/Class1.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+namespace R5T.S0024.Lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ source/R5T.S0024/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0024
+{
+    /// <summary>
+    /// Survey all local extension method base types, add to repository.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
